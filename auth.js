54770e66280c24ee5f2bea9292b4b23fb82d5123
monitor111 <lugovoy111wowa@gmail.com>
2025-11-28 22:09:43 +0200

    Продолжение4 - 1 устройство 1 пользователь

--- auth.js
+++ auth.js
@@ -7,13 +7,49 @@ function generateDeviceId() {
   return 'user_' + Math.random().toString(36).substr(2, 9) + Date.now().toString(36);
 }
 
+// Сохранение deviceId в cookie (переживет очистку кеша)
+function saveDeviceIdToCookie(deviceId) {
+  // Cookie будет жить 10 лет
+  const expires = new Date(Date.now() + 10 * 365 * 24 * 60 * 60 * 1000).toUTCString();
+  document.cookie = `deviceId=${deviceId}; expires=${expires}; path=/; SameSite=Strict`;
+}
+
+// Получение deviceId из cookie
+function getDeviceIdFromCookie() {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === 'deviceId') return value;
+  }
+  return null;
+}
+
 // Получение или создание ID устройства
 function getDeviceId() {
+  // Сначала пробуем localStorage
   let deviceId = localStorage.getItem('deviceId');
+  
+  // Если нет в localStorage - пробуем cookie
+  if (!deviceId) {
+    deviceId = getDeviceIdFromCookie();
+    if (deviceId) {
+      // Восстанавливаем в localStorage
+      localStorage.setItem('deviceId', deviceId);
+      console.log('deviceId восстановлен из cookie:', deviceId);
+    }
+  }
+  
+  // Если нигде нет - создаем новый
   if (!deviceId) {
     deviceId = generateDeviceId();
     localStorage.setItem('deviceId', deviceId);
+    saveDeviceIdToCookie(deviceId);
+    console.log('Создан новый deviceId:', deviceId);
+  } else {
+    // Убеждаемся что везде сохранено
+    saveDeviceIdToCookie(deviceId);
   }
+  
   return deviceId;
 }
 
@@ -61,6 +97,8 @@ export async function signOutUser() {
   // Очищаем все данные
   localStorage.removeItem('deviceId');
   localStorage.removeItem('displayName');
+  // Удаляем cookie
+  document.cookie = 'deviceId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 }
 
 // Функция для обновления статуса онлайн
